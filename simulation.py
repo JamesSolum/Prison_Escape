5578458655af4a4dfd9cd05c1a4160e0b8315c66
James Solum <jsolum@mccollum.enterprises>
2017-04-21 15:02:09 -0700

    added my simulation code

---
+++ simulation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+Simulation
+
+Author: James Solum
+
+This will be the main file that will build and run our simulation
+
+"""
+from player import *
+
+### Constants ###
+BORDER = 8
+
+## Player Specific Constants ##
+SQUARE_GUARD_PATROL_BORDER = 5
+GUARD_PATH = [(1,1),(2,1),(1,2),(2,2),(1,3),(0,4),(0,3),(-1,2),(-1,1),(-1,0),(-1,-1),(0,-1)]
+CHANGE_IN_PROB = 0.1
+
+
+
+
+
+def main():
+    
+
+    
+
+    def instantiatePlayers():
+        billy = billy(
+
+
+
+def __init__():
+    main()
